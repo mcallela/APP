1345c48b5acff8ce7248bd989bd90d6de970f96d
mcallela <wario.callela@gmail.com>
2021-10-02 22:38:48 -0500

    P-3

--- main.py
+++ main.py
@@ -24,21 +24,25 @@ class  MatplotlibWidget ( QMainWindow ):
 
 
     def update_graph ( self ):
+        cantidad =int(self.lineEdit.text())
+        cantidad2 =int(self.lineEdit_2.text())
 
-        fs  =  500 
-        f  =  random . randint ( 1 ,  100 ) 
+        #fs  =  500 
+        #f  =  random . randint ( 1 ,  100 ) 
+        fs=cantidad +1
+        f= cantidad2
         ts  =  1 / fs 
-        length_of_signal  =  100 
-        t  =  np . linspace ( 0 , 1 , length_of_signal )
+        length_of_signal  =  100
+        t  =  np . linspace ( 0 , 10 , length_of_signal )
         
-        cosinus_signal  =  np . cos ( 2 * np . pi * f * t ) 
+        cosinus_signal  =  np . cos ( 2 * np . pi * fs * t ) 
         sinus_signal  =  np . sin ( 2 * np . pi * f * t )
 
         self. MplWidget . canvas. axes . clear () 
         self . MplWidget . canvas . axes . plot ( t ,  cosinus_signal ) 
         self . MplWidget . canvas . axes . plot ( t ,  sinus_signal ) 
-        self . MplWidget . canvas . axes . legend(( 'cosinus' ,  'sinus' ),loc = 'upper right' ) 
-        self . MplWidget . canvas . axes . set_title ( ' Cosinus - Sinus Signal' ) 
+        self . MplWidget . canvas . axes . legend(( 'Dato1' ,  'Dato2' ),loc = 'upper right' ) 
+        self . MplWidget . canvas . axes . set_title ( ' Dato ' ) 
         self . MplWidget . canvas . draw()
         
 
